6d4835d808fafea0d67973e802b05a65df880a77
ozgur.utku <ozgur.utku@ozguryazilim.com.tr>
2021-04-27 16:04:16 +0300

    frontend edit

--- src/main/java/com/ecommerce/model/Cart.java
+++ src/main/java/com/ecommerce/model/Cart.java
@@ -8,7 +8,7 @@ import java.util.List;
 public class Cart {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column
     private Long id;
 
